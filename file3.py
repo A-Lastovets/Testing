3f15384ab99ad07c1b4b52da63c5799aedd60d0e
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2024-04-14 00:12:41 +0200

    next_changes

--- file3.py
+++ file3.py
@@ -1,2 +1,17 @@
 fruits = ["banana", "orange", "apple"]
 prices = [10, 20, 30]
+
+for name in fruits:
+    for cost in prices:
+        print(name + ':', cost)
+
+# for name, cost in zip(fruits, prices):
+#     print(name + ':', cost)
+
+# i = 0
+# while i < len(fruits):
+#     print(fruits[i] + ':', prices[i])
+#     i += 1
+
+# for index, value in enumerate(prices):
+#     print(index, value)

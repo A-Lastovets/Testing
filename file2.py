afc28e689fb7d8a2b8779749b8702c048fd51895
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2024-04-14 14:26:13 +0200

    add_set_methods

--- file2.py
+++ file2.py
@@ -6,13 +6,13 @@
 # 1) элементы с первого списка, которых нет во втором
 # 2) элементы со второго списка, которых нет в первом
 
-first_list = set([1, 6, 7, 9, 2, 3, 4])
-second_list = set([5, 2, 1, 8, 3, 7, 10])
-print(set(first_list) - set(second_list))
-print(set(second_list) - set(first_list))
+# first_list = set([1, 6, 7, 9, 2, 3, 4])
+# second_list = set([5, 2, 1, 8, 3, 7, 10])
+# print(set(first_list) - set(second_list))
+# print(set(second_list) - set(first_list))
 
-print(set(first_list).difference(second_list))
-print(set(second_list).difference(first_list))
+# print(set(first_list).difference(second_list))
+# print(set(second_list).difference(first_list))
 
 # first_list2 = []
 # second_list2 =[]
